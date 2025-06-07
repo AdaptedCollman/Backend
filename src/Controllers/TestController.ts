1ac0326a8fcb0a107633c94d19c8ac49345c4309
EladRab1106 <eladrabinovitch1106@gmail.com>
2025-06-07 15:45:08 +0300

    scores graph

--- src/Controllers/TestController.ts
+++ src/Controllers/TestController.ts
@@ -3,21 +3,23 @@ import { Test } from '../Models/TestModel';
 import { Question } from '../Models/QuestionModel';
 import { UserQuestion } from '../Models/UserQuestionModel';
 
-// Function to create a test
+
 
 export const getUserStatsFromTests = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
   try {
-    const tests = await Test.find({ user: userId, endedAt: { $exists: true } });
+    const tests = await Test.find({ user: userId, endedAt: { $exists: true } })
+      .sort({ startedAt: 1 }); // סידור לפי סדר ביצוע המבחנים
 
     if (!tests.length) {
-      res.json({
+       res.json({
         averageScore: 0,
         totalQuestions: 0,
         averageDuration: "0s",
+        scores: [],
       });
-      return 
+      return
     }
 
     const totalScore = tests.reduce((sum, test) => sum + (test.score ?? 0), 0);
@@ -31,7 +33,6 @@ export const getUserStatsFromTests = async (req: Request, res: Response) => {
     const averageScore = totalScore / tests.length;
     const averageDurationSec = totalDuration / tests.length;
 
-    // ✅ פונקציה לעיצוב הזמן
     const formatDuration = (seconds: number): string => {
       if (seconds >= 3600) {
         const h = Math.floor(seconds / 3600);
@@ -46,10 +47,13 @@ export const getUserStatsFromTests = async (req: Request, res: Response) => {
       }
     };
 
+    const scores = tests.map((t) => t.score ?? 0); // מערך של ציונים
+
      res.json({
       averageScore: averageScore.toFixed(1),
       totalQuestions: tests.length * 63,
       averageDuration: formatDuration(averageDurationSec),
+      scores,
     });
     return
   } catch (err) {
@@ -57,6 +61,7 @@ export const getUserStatsFromTests = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to fetch test statistics" });
   }
 };
+
 
 export const createTest = async (req: Request, res: Response) => {
   try {
